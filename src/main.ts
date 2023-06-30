a57b94c71e91fe390c3d499348569cbd00684c1a
Zaporojan Victor <victor.zaporojan0@gmail.com>
2023-06-30 15:29:04 +0100

    added particles js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,83 @@
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import './style.scss'
+import { tsParticles } from "tsparticles-engine";
+
+(async () => {
+  //@ts-ignore
+  await loadTrianglesPreset(tsParticles); // this is required only if you are not using the bundle script
+
+  await tsParticles.load("tsparticles", {
+    particles: {
+      "number": {
+        "value": 35,
+        "density": {
+          "enable": true,
+          "value_area": 500
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 0.5,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 10,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+
+    }
+  });
+})();
 
 const app = document.querySelector<HTMLDivElement>('#app');
 if (app) {

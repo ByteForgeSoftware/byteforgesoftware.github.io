a76c9a60c870addda8d1346374f66ce6ff0dd89b
zapobyte <victor.zaporojan0@gmail.com>
2024-04-25 00:13:05 +0100

    redesign webpage

--- src/main.ts
+++ src/main.ts
@@ -1,144 +1,71 @@
-import 'bootstrap';
-import 'bootstrap/scss/bootstrap.scss';
-import './style.scss'
-import { tsParticles } from "tsparticles-engine";
+import './style.scss';
 
-(async () => {
-  //@ts-ignore
-  await loadTrianglesPreset(tsParticles); // this is required only if you are not using the bundle script
+const canvas: HTMLCanvasElement = document.getElementById('matrixCanvas') as HTMLCanvasElement;
+const context: CanvasRenderingContext2D = canvas.getContext('2d')!;
 
-  await tsParticles.load("tsparticles", {
-    particles: {
-      "number": {
-        "value": 35,
-        "density": {
-          "enable": true,
-          "value_area": 500
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 0.5,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 10,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 2,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
+let fontSize: number = 16;
+let columns: number;
+const characters: string[] = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()_+");
+let drops: number[];
 
-    }
-  });
-})();
-
-const app = document.querySelector<HTMLDivElement>('#app');
-if (app) {
-  const template = `
-  
-  <header>
-    <nav class="navbar navbar-expand-lg bg-transparent text-light">
-      <div class="container-fluid">
-        <a class="navbar-brand text-white d-inline-flex align-items-center" href="#">
-        <img src="/logo.png" class="logo me-3"/>
-        <span>ByteForgeSoftware</span>
-        </a>
-        <button class="navbar-toggler border-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0  text-light">
-            <li class="nav-item">
-              <a href="mailto:byte.forge.software@gmail.com" class="nav-link text-white  align-items-center d-flex">
-              <i class="fa-regular fa-envelope me-1"></i>
-              <span>byte.forge.software@gmail.com</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
-  <main class="container">
-    <div class="wrapper text-white">
-      <span>Forging the Future of </span>
-      <div class="stack" style="--stacks: 3;">
-        <span style="--index: 0;">Software</span>
-        <span style="--index: 1;">Software</span>
-        <span style="--index: 2;">Software</span>
-      </div>
-    <p class="text-end w-100">Excellence!</p>
-    </div>
-</main>
-<footer class="container-fluid">
-  <div class="row">
-    <ul class="social-media-list col-12 text-center">
-      <li class="text-white d-inline-flex  mx-2">
-        <a href="#" class="nav-link text-white  align-items-center d-flex">
-          <i class="fa-brands fa-linkedin"></i>
-        </a>
-      </li>
-      <li class="text-white d-inline-flex mx-2">
-        <a href="mailto:byte.forge.software@gmail.com" class="nav-link text-white  align-items-center d-flex">
-          <i class="fa-regular fa-envelope me-1"></i>
-        </a>
-      </li>
-      <li class="text-white d-inline-flex  mx-1">
-        <a href="https://github.com/ByteForgeSoftware" class="nav-link text-white  align-items-center d-flex">
-          <i class="fa-brands fa-github"></i>
-        </a>
-      </li>
-    </ul>
-    <p class="text-center text-white col-12"><small> code with &#x2665;  ${new Date().getFullYear()} @ <a href="/">ByteForgeSoftware</a></small></p>
-  </div>
-</footer>
-`;
-  app.innerHTML = template;
+function setupCanvas(): void {
+  canvas.width = window.innerWidth / 2;
+  canvas.height = window.innerHeight;
+  fontSize = Math.floor(canvas.width / 100);  // Adjust font size based on width
+  columns = Math.floor(canvas.width / fontSize);
+  drops = Array.from({ length: columns }).fill(0) as number[];
 }
+
+function draw(): void {
+  context.fillStyle = 'rgba(3, 3, 3, 0.05)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = '#0F0'; // Green text for the falling characters
+  context.font = `${fontSize}px monospace`;
+
+  for (let i = 0; i < drops.length; i++) {
+    const text: string = characters[Math.floor(Math.random() * characters.length)];
+    context.fillText(text, i * fontSize, drops[i] * fontSize);
+
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
+      drops[i] = 0;
+    } else {
+      drops[i]++;
+    }
+  }
+}
+
+function initTerminalEffect(): void {
+  const terminal: HTMLElement = document.getElementById('terminal')!;
+  const text: string = "ByteForgeSoftware";
+  const typingDelay: number = 75;
+  let index: number = 0;
+  const data = document.getElementById('data');
+  function typeText(): void {
+    if (index < text.length) {
+      terminal.textContent += text[index++];
+      setTimeout(typeText, typingDelay);
+    } else {
+      if (data && data.style.display === 'none') {
+        setTimeout(() => {
+          data.style.display = 'table';
+          const currentYear = document.getElementById('currentYear');
+          if(currentYear){
+            currentYear.innerHTML = new Date().getFullYear().toString();
+          }
+        }, 250)
+      }
+      terminal.innerHTML += '<span class="blink">_</span>';
+    }
+  }
+
+  setTimeout(typeText, typingDelay);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initTerminalEffect();
+  setupCanvas();
+  setInterval(draw, 50);
+});
+
+window.addEventListener('resize', setupCanvas);
